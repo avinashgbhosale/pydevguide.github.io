b60f38db978c77cd87c10b69170c9a03c176ff3f
avinashgbhosale <bhosaleavinash22@gmail.com>
2019-10-20 22:05:45 +0530

    first commit

---
+++ precache-manifest.ebddbda6a7e902061bf76b942e4d2bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6272f78e91466f7c03d2a7c6298e01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97f5cab3894d079b601b",
+    "url": "/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "24b107a54c7b0913a51e",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "97f5cab3894d079b601b",
+    "url": "/static/js/2.acaff463.chunk.js"
+  },
+  {
+    "revision": "24b107a54c7b0913a51e",
+    "url": "/static/js/main.94887dd5.chunk.js"
+  },
+  {
+    "revision": "49dd5413ec63f342faf7",
+    "url": "/static/js/runtime~main.30102f7e.js"
+  }
+]);
