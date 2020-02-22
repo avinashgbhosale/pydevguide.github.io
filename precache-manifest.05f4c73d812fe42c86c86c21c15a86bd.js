90ada73ea56a144a55214dc4181a6bf3277d23fe
avinashgbhosale <bhosaleavinash22@gmail.com>
2020-02-22 19:52:58 +0530

    22-02 build

---
+++ precache-manifest.05f4c73d812fe42c86c86c21c15a86bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87285bfa3a40bc2fd88f85fa13560785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c650c3cf7800d94f228",
+    "url": "/static/css/2.6e673546.chunk.css"
+  },
+  {
+    "revision": "0d62f890e8143ae9b1a2",
+    "url": "/static/css/main.64a5eab9.chunk.css"
+  },
+  {
+    "revision": "4c650c3cf7800d94f228",
+    "url": "/static/js/2.ec8ad697.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.ec8ad697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d62f890e8143ae9b1a2",
+    "url": "/static/js/main.923847ee.chunk.js"
+  },
+  {
+    "revision": "0b2373293994237f85e7",
+    "url": "/static/js/runtime-main.0236b42a.js"
+  }
+]);
